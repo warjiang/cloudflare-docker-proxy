c498b4c27dedabd76207a1a1e59d82c5dfbb6ca9
warjiang <1096409085@qq.com>
2024-07-16 23:35:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ const dockerHub = "https://registry-1.docker.io";
 const routes = {
   // production
   "docker.maoding.asia": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  //"quay.libcuda.so": "https://quay.io",
+  //"gcr.libcuda.so": "https://gcr.io",
+  //"k8s-gcr.libcuda.so": "https://k8s.gcr.io",
+  //"k8s.libcuda.so": "https://registry.k8s.io",
+  //"ghcr.libcuda.so": "https://ghcr.io",
+  //"cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  //"ecr.libcuda.so": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.maoding.asia": dockerHub,
 };
 
 function routeByHosts(host) {
